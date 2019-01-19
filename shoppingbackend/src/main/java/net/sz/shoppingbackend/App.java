4c8a9c007c6f474ca2e1a61993707d78733f7e5c
Shafi Zaman <shafizaman@hotmail.com>
2019-01-19 11:55:03 -0600

    SZ project setup completed successfully.

---
+++ shoppingbackend/src/main/java/net/sz/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.sz.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
